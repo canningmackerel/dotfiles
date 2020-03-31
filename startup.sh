ec4b1384b236e70f7c1e6b9d609cd5ed790060bb
canningmackerel <sugihara443@gmail.com>
2020-03-31 21:51:55 +0900

    add start up script

---
+++ startup.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# install homebrew
+# https://brew.sh/index_ja
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+brew install python vim peco 
+
+# install vim-plug
+# https://github.com/junegunn/vim-plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
