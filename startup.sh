c30476594ba226a9a669016a10f66ca6b2a43437
canningmackerel <sugihara443@gmail.com>
2020-03-31 21:55:50 +0900

    add symbolic linc

--- startup.sh
+++ startup.sh
@@ -9,3 +9,5 @@ brew install python vim peco
 # install vim-plug
 # https://github.com/junegunn/vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+ln -s ./.vimrc ~/.vimrc
